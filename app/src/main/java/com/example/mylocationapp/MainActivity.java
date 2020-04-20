2f87f532c7ee57079185470276b6f170701ae5cf
sebastianbocul <seba23495@gmail.com>
2020-04-20 19:00:27 +0200

    Location map has been added

--- app/src/main/java/com/example/mylocationapp/MainActivity.java
+++ app/src/main/java/com/example/mylocationapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mylocationapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -17,6 +18,12 @@ import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -24,11 +31,15 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     Button btLocation;
     TextView textView1,textView2,textView3,textView4,textView5;
     FusedLocationProviderClient fusedLocationProviderClient;
+
+    private static final int REQUEST_CODE = 44;
+
+    public List<Address> addresses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                     getLocation();
                 }
                 else {
-                    ActivityCompat.requestPermissions(MainActivity.this, new  String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+                    ActivityCompat.requestPermissions(MainActivity.this, new  String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
                 }
             }
         });
@@ -66,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 if(location!=null){
                     try {
                         Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
-                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                        addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
 
                         textView1.setText("Lattitude: " + addresses.get(0).getLatitude());
                         textView2.setText("Longitude: " + addresses.get(0).getLongitude());
@@ -74,11 +85,37 @@ public class MainActivity extends AppCompatActivity {
                         textView4.setText("Locality: " + addresses.get(0).getLocality());
                         textView5.setText("Address: " + addresses.get(0).getAddressLine(0));
 
+                        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+                        supportMapFragment.getMapAsync(MainActivity.this);
+
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             }
         });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(addresses.get(0).getLatitude(),addresses.get(0).getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here.");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+        googleMap.addMarker(markerOptions);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_CODE:
+                if(grantResults.length>0 && grantResults[0]  == PackageManager.PERMISSION_GRANTED){
+                    getLocation();
+                }
+                break;
+
+
+        }
     }
 }
