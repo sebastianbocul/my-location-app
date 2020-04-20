8fdafd4187966336ea5852511793824cc890fb9d
sebastianbocul <seba23495@gmail.com>
2020-04-20 15:52:45 +0200

    Showing my location info

--- app/src/main/java/com/example/mylocationapp/MainActivity.java
+++ app/src/main/java/com/example/mylocationapp/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.mylocationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btLocation;
+    TextView textView1,textView2,textView3,textView4,textView5;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btLocation = (Button) findViewById(R.id.bt_location);
+        textView1 =(TextView) findViewById(R.id.text_view1);
+        textView2 =(TextView) findViewById(R.id.text_view2);
+        textView3 =(TextView) findViewById(R.id.text_view3);
+        textView4 =(TextView) findViewById(R.id.text_view4);
+        textView5 =(TextView) findViewById(R.id.text_view5);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        btLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //checking permission
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                    getLocation();
+                }
+                else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new  String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+                }
+            }
+        });
+
+    }
+
+    private void getLocation() {
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if(location!=null){
+                    try {
+                        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+
+                        textView1.setText("Lattitude: " + addresses.get(0).getLatitude());
+                        textView2.setText("Longitude: " + addresses.get(0).getLongitude());
+                        textView3.setText("Country Name: " + addresses.get(0).getCountryName());
+                        textView4.setText("Locality: " + addresses.get(0).getLocality());
+                        textView5.setText("Address: " + addresses.get(0).getAddressLine(0));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
